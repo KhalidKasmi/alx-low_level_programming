cce7116c8ae6bd27e4439928d0c3318a10958faf
Khalid <kasmikhalid190@gmail.com>
2023-04-27 16:23:46 -0700

    responses2

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+/**
+ * list_len: is a function to give the length of a linked list_len
+ * *h: a pointer to the head of the list 
+ * 
+ * return: the number of elements
+ **/
+ 
+ 
+
+size_t list_len(const list_t *h){
+    size_t l = 0;
+    while (h!= NULL ){
+        l++;
+        h = h->next ;
+         }
+         return l;
+}
