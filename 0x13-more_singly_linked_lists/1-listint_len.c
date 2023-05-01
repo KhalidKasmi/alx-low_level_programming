d416b943c7867c427834ece65d356e713e14f6be
Khalid <kasmikhalid190@gmail.com>
2023-05-01 08:52:14 -0700

    answers3

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+
+#include "lists.h"
+
+/**
+* listint_len - counts elements of a linked list 
+* @h :  linked list of type listint_t to traverse
+*
+* return : number of elements
+* /
+
+size_t listint_len(const listint_t *h)
+{
+  const listint_t *temp;
+  size_t count = 0;
+  temp = h;
+  while (temp != NULL)
+    {
+      temp = temp->next;
+      count ++;
+    }
+    return count;
+}
