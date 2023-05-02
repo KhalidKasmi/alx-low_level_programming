44de13721fd963f129c657ee225a66dfbdff601d
Khalid <kasmikhalid190@gmail.com>
2023-05-01 20:33:59 -0700

    last responses

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,36 +1,34 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end- add a new node in the end of the list
- * @head : linked list to traverse
- * @n : the value to insert
- * 
- * Return : the address of the new node or NULL 
+* add_nodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the first element in the list
+ * @n: data to insert in the new element
+ *
+ * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint_end (listint_t ** head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-  listint_t *new;
-  new = malloc (sizeof (listint_t));
-  if (new == NULL)
-    {
-      return (NULL);
-    }
+	listint_t *new;
+	listint_t *temp = *head;
 
-  new->n = n;
-  new->next = NULL;
-  if (*head == NULL)
-    {
-      *head = new;
-      return (new);
-    }
-  listint_t *temp = *head;
-  while (temp->next != NULL)
-    {
-      temp = temp->next;
-    }
-  temp->next = new;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 
-  return (new);
+	new->n = n;
+	new->next = NULL;
 
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
 }
-
